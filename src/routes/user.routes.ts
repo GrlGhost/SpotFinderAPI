a8b3269333aa429d99a5816b40b33a77d40db2fa
Franco B <franco.barravecchia2002@gmail.com>
2023-03-29 11:01:06 -0300

    user routes correction

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -3,13 +3,13 @@ import { addUser, deleteUser, modifieUser } from "../controllers/user.controller
 
 const usersRouter = Router();
 
-usersRouter.route('/add')
-    .put(addUser)//for adding somthing new
+usersRouter.route('/')
+    .post(addUser)//for adding somthing new
 
 
 //Example: amd/pepito33@gmail this implies a delete or modification in pepito33    
 usersRouter.route('/:userMail')
-    .post(modifieUser)//post to modify
+    .put(modifieUser)//post to modify
     .delete(deleteUser)
 
 export default usersRouter
