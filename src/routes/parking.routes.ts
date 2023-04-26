860bbfbf10fa56ea326052f887d055163f92b3cd
lucaslovaglio <lucas.lovaglio@ing.austral.edu.ar>
2023-04-26 11:46:02 -0300

    port 3001 and some corrections (parcial)

--- src/routes/parking.routes.ts
+++ src/routes/parking.routes.ts
@@ -12,7 +12,7 @@ parkingRouter.route('/')
     .post(addParking);
 
 parkingRouter.route('/parkingsFromArea')
-    .get(getParkingsFromArea);
+    .post(getParkingsFromArea);
 
 parkingRouter.route('/events')
     .post(generateClient);
