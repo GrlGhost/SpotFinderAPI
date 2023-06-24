3b7e757ceb2cc227c703b1077df3894ec57b3fcc
Franco B <franco.barravecchia2002@gmail.com>
2023-06-24 19:31:01 -0300

    pricexminute added

--- src/controllers/parking/parking.controller.ts
+++ src/controllers/parking/parking.controller.ts
@@ -16,11 +16,11 @@ export async function addParking(req: Request, res: Response, next: NextFunction
         const newPark: parking = req.body;
 
         const conn = connect();
-        await conn.query('INSERT INTO parkings(geog, name, capacity, openHour, closeHour, phone, rating, ownermail)'
-            + 'VALUES($1, $2, $3, $4, $5, $6, $7, $8)',
+        await conn.query('INSERT INTO parkings(geog, name, capacity, openHour, closeHour, phone, rating, ownermail, attendance, pricexminute)'
+            + 'VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)',
             ['SRID=4326;POINT(' + newPark.lon + ' ' + newPark.lat + ')', newPark.name, newPark.capacity,
             newPark.openHour ? newPark.openHour : null, newPark.closeHour ? newPark.closeHour : null,
-            newPark.phone ? newPark.phone : null, 0, newPark.ownerMail]);
+            newPark.phone ? newPark.phone : null, 0, newPark.ownerMail, null, newPark.pricexminute]);
         return res.status(HttpStatus.OK).json({ response: 'Succesfuly created parking' })
     } catch (err) {
         return next(err);
@@ -32,7 +32,7 @@ export async function modifieParking(req: Request, res: Response, next: NextFunc
         const parkingID = req.params.id;
         const updates = req.body;
 
-        const allowedColumns: string[] = ['name', 'capacity', 'openHour', 'closeHour', 'phone', 'ownerMail'];
+        const allowedColumns: string[] = ['name', 'capacity', 'openHour', 'closeHour', 'phone', 'ownerMail', 'pricexminute'];
         const validUpdates: validUpdates = {};
 
         Object.keys(updates).forEach(key => {
@@ -80,7 +80,7 @@ export async function getParkingsOfOwner(req: Request, res: Response, next: Next
         const conn = connect();
         const qres: QueryResult = await conn.query('SELECT gid AS id, ST_X(ST_Transform(geog::geometry, 4326)) ' +
             'longitude, ST_Y(ST_Transform(geog::geometry, 4326)) latitude, name, capacity, openhour,' +
-            ' closehour, phone, rating, attendance FROM parkings WHERE ownerMail = $1', [usermail]);
+            ' closehour, phone, rating, attendance, pricexminute FROM parkings WHERE ownerMail = $1', [usermail]);
 
         res.status(HttpStatus.OK).send({ 'parkingsOwned': qres.rows });
     } catch (err) {
@@ -97,7 +97,7 @@ export async function getParkingsFromArea(req: Request, res: Response, next: Nex
         //min_lon, min_lat, max_lon, max_lat, 4326
         const result = await conn.query('SELECT gid AS id, ST_X(ST_Transform(geog::geometry, 4326)) longitude, ' +
             'ST_Y(ST_Transform(geog::geometry, 4326)) latitude, name, capacity, openhour, closehour, phone, ' +
-            'rating, attendance FROM parkings WHERE ST_Intersects(geog, ST_MakeEnvelope($1, $2, $3, $4, 4326))',
+            'rating, attendance, pricexminute FROM parkings WHERE ST_Intersects(geog, ST_MakeEnvelope($1, $2, $3, $4, 4326))',
             [area.mLon, area.mLat, area.MLon, area.MLat]);
 
         res.status(HttpStatus.OK).json(result.rows);
@@ -110,6 +110,7 @@ export async function getParkingsFromArea(req: Request, res: Response, next: Nex
 //TODO: this endpoint should not be so open to get users for mail user. The user should be pass by qr (previos endpoint)
 //TODO: before setting down the attendance without mail, should check if there was someone that entered without mail, 
 //could be an extra column in parking
+//TODO: create a full table of price x time
 export async function modifieAttendance(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
     try {
         if (!req.body.userMail) throw new BadRequestError(true, 'userMail');
@@ -118,10 +119,8 @@ export async function modifieAttendance(req: Request, res: Response, next: NextF
         await modAttendanceAux(parseInt(req.params.id), userMail, increase, false, req.body.appClients);
 
         if (req.body.automatedExit){
-            //TODO: complete this method, shold get the amount to pay previously
-            throw new Error('The method must be completed')
             const conn = connect();
-            const qRes: QueryResult = await conn.query('SELECT ownermail FROM parkings WHERE gid = $1', [req.params.id]);
+            const qRes: QueryResult = await conn.query('SELECT ownermail, pricexminute FROM parkings WHERE gid = $1', [req.params.id]);
             if (qRes.rowCount === 0) throw new NotFoundError(true, 'id');
 
             const qRes2: QueryResult = await conn.query('SELECT entryhourutc FROM user_parked_at WHERE usermail = $1', [userMail]);
@@ -136,7 +135,7 @@ export async function modifieAttendance(req: Request, res: Response, next: NextF
             let minutes = time[1]-timeEntry[1];
             minutes = (time[0]-timeEntry[0])*60;
 
-            sendMoney(userMail, qRes.rows[0].ownermail, 100*minutes);   
+            sendMoney(userMail, qRes.rows[0].ownermail, qRes.rows[0].pricexminute*minutes);   
         }
 
         res.status(HttpStatus.OK).send('correctly updated');
@@ -150,7 +149,7 @@ export async function getAllInfoOfParking(req: Request, res: Response, next: Nex
         const conn = connect();
         const response = await conn.query('SELECT gid AS id, ST_X(ST_Transform(geog::geometry, 4326)) longitude, ' +
         'ST_Y(ST_Transform(geog::geometry, 4326)) latitude, name, capacity, openhour, closehour, phone, ' +
-        'rating, attendance FROM parkings WHERE gid = $1', [req.params.id]);
+        'rating, attendance, pricexminute FROM parkings WHERE gid = $1', [req.params.id]);
         
         if (response.rowCount == 0) throw new NotFoundError(true, 'id');
 
--- src/interfaces/parking.interface.ts
+++ src/interfaces/parking.interface.ts
@@ -8,4 +8,5 @@ export interface parking {
     closeHour?: string;
     phone?: string;
     id?: number;
+    pricexminute?: number;
 }
