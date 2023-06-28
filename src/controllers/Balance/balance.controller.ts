0033346d54fe8cc0086d1d8a45568eafeff35976
Franco B <franco.barravecchia2002@gmail.com>
2023-06-28 00:22:09 -0300

    fix balance get

--- src/controllers/Balance/balance.controller.ts
+++ src/controllers/Balance/balance.controller.ts
@@ -28,7 +28,7 @@ export async function getBalance(req: Request, res: Response, next: NextFunction
         const conn = connect();
         const qRes = await conn.query('SELECT balance_c FROM balance WHERE mail = $1', [req.params.mail]);
         if (qRes.rowCount === 0) res.status(HttpStatus.OK).send({balance: 0, message: 'operation made succesefully'});
-        else res.status(HttpStatus.OK).send({balance: qRes.rows[0].balance, message: 'operation made succesfully'});
+        else res.status(HttpStatus.OK).send({balance: qRes.rows[0].balance_c, message: 'operation made succesfully'});
     }catch(err){
         next(err);
     }
